095cc928a38b5cd75838fcc147afa729ad9ba1c8
최우석 <choiwuseck@choeuseog-ui-MacBookAir.local>
2024-12-03 19:19:21 +0900

    시간까지 구현

--- js/today.js
+++ js/today.js
@@ -1,23 +1,25 @@
-const dayOfWeek = ["일", "월","화","수","목","금","토"];
+const dayOfWeek = ["일", "월", "화", "수", "목", "금", "토"];
 const todayDayShow = document.querySelector(".today-date-show div");
 
-function curtime(){
+function curtime() {
     const curDate = new Date();
-    let displayDate = `지금: ${curDate.getFullYear()}년 ${curDate.getMonth()+1}월 ${curDate.getDate()}일 ${dayOfWeek[curDate.getDay()]}요일`
-    const curHour = curDate.getHours();
-    let displayHour = curHour%12;
-    if(displayHour === 0){
-        displayHour = 12;
-    }
 
-    if(curHour < 12){
-        displayDate += ` 오전 ${displayHour}시`
-    }
-    else{
-        displayDate += ` 오후 ${displayHour}시`
-    }
-    
+    const year = curDate.getFullYear();
+    const month = curDate.getMonth() + 1; 
+    const date = curDate.getDate();
+    const day = dayOfWeek[curDate.getDay()];
+    const curHour = curDate.getHours();
+    const curMinute = curDate.getMinutes();
+    const curSecond = curDate.getSeconds();
+
+    let displayHour = curHour % 12 || 12;  
+    let period = curHour < 12 ? "오전" : "오후";
+
+    let displayDate = `지금: ${year}년 ${month}월 ${date}일 ${day}요일`;
+    displayDate += ` ${period} ${displayHour}시 ${curMinute}분 ${curSecond}초`;
+    displayDate += ` KST(UTC+09:00)`;
+
     todayDayShow.innerText = displayDate;
 }
-
-setTimeout(curtime,500);
+curtime();
+setInterval(curtime, 999);
